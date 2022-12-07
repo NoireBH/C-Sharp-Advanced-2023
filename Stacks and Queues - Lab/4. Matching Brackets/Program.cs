c8db68f5edb4e6c4be70ce00d18919a6052628f7
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2022-12-07 20:21:09 +0200

    added solution to projects

---
+++ Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            var indexes = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    indexes.Push(i);
+                }
+
+                else if (expression[i] == ')')
+                {
+                    int openBracketIndex = indexes.Pop();
+                    int closeBracketIndex = i;
+                    Console.WriteLine(expression.Substring(openBracketIndex,closeBracketIndex - openBracketIndex + 1));
+                }
+            }
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            var evenNumbers = new Queue<int>();
+            foreach (var number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    evenNumbers.Enqueue(number);
+                }
+            }
+
+            Console.WriteLine(String.Join(",",evenNumbers));
+
+
+        }
+    }
+}
