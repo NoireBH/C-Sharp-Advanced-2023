1d5294815ef1ee63af546c04d9de566a8df6d431
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2022-12-07 15:25:06 +0200

    added solution to projects

---
+++ Stacks and Queues - Lab/2. Stack Sum/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int>stackedNums = new Stack<int>();
+
+            foreach (var num in numbers)
+            {
+                stackedNums.Push(num);
+            }        
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                string[] cmd = input.Split();
+                string command = cmd[0].ToLower();
+
+                if (command == "end")
+                {
+                    break;
+                }
+                else if (command == "add")
+                {
+                    int n1 = int.Parse(cmd[1]);
+                    int n2 = int.Parse(cmd[2]);
+                    stackedNums.Push(n1);
+                    stackedNums.Push(n2);
+                }
+
+                else if (command == "remove")
+                {
+                    int elementsToRemove = int.Parse(cmd[1]);
+
+                    if (elementsToRemove <= stackedNums.Count)
+                    {
+                        for (int i = 0; i < elementsToRemove; i++)
+                        {
+                            stackedNums.Pop();
+                        }
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine("Sum: " + stackedNums.Sum());
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numAndOps = Console.ReadLine()
+                .Split();
+            
+            Stack<string> stack = new Stack<string>(numAndOps.Reverse());
+            int sum = 0;
+            while (stack.Count > 1)
+            {
+                int n1 = int.Parse(stack.Pop());
+                string operation = stack.Pop();
+                int n2 = int.Parse(stack.Pop());
+
+                switch (operation)
+                {
+                    case "+": 
+                        stack.Push((n1 + n2).ToString()); 
+                        break;
+
+                    case "-": 
+                        stack.Push((n1 - n2).ToString()); 
+                        break;
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
