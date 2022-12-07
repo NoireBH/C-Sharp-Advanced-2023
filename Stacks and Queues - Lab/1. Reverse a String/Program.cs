aea0eac88722f64dd28d50fbd0480e45c9a03a34
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2022-12-07 12:01:20 +0200

    added a project

---
+++ Stacks and Queues - Lab/1. Reverse a String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace _1._Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack reversedString = new Stack();
+
+            foreach (var letter in input)
+            {
+                reversedString.Push(letter);
+            }
+
+            while (reversedString.Count > 0)
+            {
+                Console.Write(reversedString.Pop());
+            }
+            
+            
+        }
+    }
+}
