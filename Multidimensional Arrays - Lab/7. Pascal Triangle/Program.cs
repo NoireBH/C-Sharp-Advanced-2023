d8c8cb71b9f6e7eb58e09f6b7b22efda533e5d25
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2022-12-13 18:29:53 +0200

    Added solution to project

---
+++ Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _7._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            long[][] triangle = new long[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                triangle[i] = new long[i + 1];
+                triangle[i][0] = 1;
+                triangle[i][triangle[i].Length - 1] = 1;
+
+                for (int j = 1; j < triangle[i].Length - 1; j++)
+                {
+                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                Console.WriteLine(String.Join(" ", triangle[i]));
+            }
+
+           
+        }
+    }
+}
