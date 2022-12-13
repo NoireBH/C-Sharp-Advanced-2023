9bf688c4f8c7db9dda59c845a642832b72243929
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2022-12-13 15:04:48 +0200

    added solutions to projects

---
+++ Multidimensional Arrays - Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[sizeOfMatrix, sizeOfMatrix];
+            int sum = 0;
+            for (int i = 0; i < sizeOfMatrix; i++)
+            {
+                int[] numbersPerRow = Console.ReadLine().Split()
+                    .Select(int.Parse).ToArray();
+
+                for (int j = 0; j < sizeOfMatrix; j++)
+                {
+                    matrix[i, j] = numbersPerRow[j];
+                }
+            }
+
+            for (int row = 0; row < sizeOfMatrix; row++)
+            {
+                sum += matrix[row, row];
+
+            }
+            Console.WriteLine(sum); 
+        }
+    }
+}
---
+++ Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Symbol_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowAndCols = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[rowAndCols, rowAndCols];
+
+            for (int i = 0; i < rowAndCols; i++)
+            {
+                char[] characters = Console.ReadLine()
+                    .ToCharArray();
+                    
+
+                for (int j = 0; j < rowAndCols; j++)
+                {
+                    matrix[i, j] = characters[j];
+                }
+            }
+
+            char characterNeeded = char.Parse(Console.ReadLine());
+            bool foundMatch = false;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                if (foundMatch)
+                {
+                    break;
+                }
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row,col] == characterNeeded)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        foundMatch = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!foundMatch)
+            {
+                Console.WriteLine($"{characterNeeded} does not occur in the matrix ");
+            }
+
+
+        }
+    }
+}
