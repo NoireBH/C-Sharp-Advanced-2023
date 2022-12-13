772e740c1407ef920ca36c3db0df598970fd90e6
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2022-12-13 17:30:02 +0200

    Added solutions to projects

--- Multidimensional Arrays - Lab/5. Square With Maximum Sum/Program.cs
+++ Multidimensional Arrays - Lab/5. Square With Maximum Sum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _5._Square_With_Maximum_Sum
 {
@@ -6,7 +7,45 @@ namespace _5._Square_With_Maximum_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numberOfRowsANdColumns = Console.ReadLine().Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[numberOfRowsANdColumns[0], numberOfRowsANdColumns[1]];
+            int maxSum = int.MinValue;
+            int sum = 0;
+            int maxRow = 0;
+            int maxCol = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] elementsOfMatrix = Console.ReadLine().Split(", ")
+                                      .Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = elementsOfMatrix[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    sum = matrix[row, col] + matrix[row, col + 1] + 
+                        matrix[row + 1, col] + matrix[row + 1, col + 1];
+
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{matrix[maxRow,maxCol]} {matrix[maxRow, maxCol + 1]}");
+            Console.WriteLine($"{matrix[maxRow + 1, maxCol]} {matrix[maxRow + 1, maxCol + 1]}");
+            Console.WriteLine(maxSum);
         }
     }
 }
---
+++ Multidimensional Arrays - Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] col = Console.ReadLine()
+                     .Split()
+                     .Select(int.Parse)
+                     .ToArray();
+                matrix[i] = col;
+            }
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] cmd = input.Split();
+                string command = cmd[0];
+                int row = int.Parse(cmd[1]);
+                int col = int.Parse(cmd[2]);
+                int value = int.Parse(cmd[3]);
+
+                if (row < 0 || row >= matrix.Length || col < 0 || col >= matrix.Length)
+                {
+                     Console.WriteLine("Invalid coordinates");
+                }
+
+                else
+                {
+                    if (command == "Add")
+                    {
+                        matrix[row][col] += value;
+                    }
+
+                    else if (command == "Subtract")
+                    {
+                        matrix[row][col] -= value;
+                    }
+                }
+            }
+         
+            foreach (int[] row in matrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+
+        }
+    }
+}
