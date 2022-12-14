98f306e2d1bd7f7d28a9f3c2dc9b4fc15867053f
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2022-12-14 18:17:34 +0200

    Added solutions to projects

---
+++ Multidimensional Arrays - Exercise/6. Jagged Array Manipulator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Manipulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] col = Console.ReadLine()
+                     .Split()
+                     .Select(int.Parse)
+                     .ToArray();
+                matrix[row] = col;
+            }
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                if (matrix[row].Length == matrix[row + 1].Length)
+                {
+                    matrix[row] = matrix[row].Select(x => x * 2).ToArray();
+                    matrix[row + 1] = matrix[row + 1].Select(x => x * 2).ToArray();
+                }
+
+                else
+                {
+                    matrix[row] = matrix[row].Select(x => x / 2).ToArray();
+                    matrix[row + 1] = matrix[row + 1].Select(x => x / 2).ToArray();
+                }
+            }
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] cmd = input.Split();
+                string command = cmd[0];
+                int row = int.Parse(cmd[1]);
+                int col = int.Parse(cmd[2]);
+                int value = int.Parse(cmd[3]);
+
+                if (row >= 0 && row < rows && col >= 0 && col < matrix[row].Length)
+                {
+                    if (command == "Add")
+                    {
+                        matrix[row][col] += value;
+                    }
+
+                    else if (command == "Subtract")
+                    {
+                        matrix[row][col] -= value;
+                    }
+
+                }
+            }
+
+            foreach (int[] row in matrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
---
+++ Multidimensional Arrays - Exercise/7. Knight Game/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+
+namespace _7._Knight_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowAndCol = int.Parse(Console.ReadLine());
+            char[,] board = new char[rowAndCol, rowAndCol];
+
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+               string knightsAndZeroes = Console.ReadLine();
+
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    board[row, col] = knightsAndZeroes[col];
+                }
+            }
+            int removedKnights = 0;
+
+            while (true)
+            {
+                int maxAttacks = 0;
+                int knightRow = 0;
+                int knightCol = 0;
+
+                for (int row = 0; row < rowAndCol; row++)
+                {
+                    for (int col = 0; col < rowAndCol; col++)
+                    {
+                        int currentAttacks = 0;
+
+                        if (board[row, col] != 'K')
+                        {
+                            continue;
+                        }
+
+                        if (IsInside(board, row - 2, col + 1) && board[row - 2, col + 1] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+
+                        if (IsInside(board, row - 1, col + 2) && board[row - 1, col + 2] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+
+                        if (IsInside(board, row + 1, col + 2) && board[row + 1, col + 2] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+
+                        if (IsInside(board, row + 2, col + 1) && board[row + 2, col + 1] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+
+                        if (IsInside(board, row + 2, col - 1) && board[row + 2, col - 1] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+
+                        if (IsInside(board, row + 1, col - 2) && board[row + 1, col - 2] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+
+                        if (IsInside(board, row - 1, col - 2) && board[row - 1, col - 2] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+
+                        if (IsInside(board, row - 2, col - 1) && board[row - 2, col - 1] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+
+
+                        if (currentAttacks > maxAttacks)
+                        {
+                            maxAttacks = currentAttacks;
+                            knightRow = row;
+                            knightCol = col;
+
+                        }
+                    }
+                }
+
+                if (maxAttacks == 0)
+                {
+                    Console.WriteLine(removedKnights);
+                    break;
+                }
+
+                else
+                {
+                    board[knightRow, knightCol] = '0';
+                    removedKnights++;
+                }
+
+            }
+        }
+
+        private static bool IsInside(char[,] board, int row, int col)
+        {
+            return row >=0 && row < board.GetLength(0)
+                && col >=0 && col < board.GetLength(1);
+        }
+
+        private static void FillMatrix(int rowAndCol, char[,] matrix, string knightsAndZeroes)
+        {
+            
+        }
+    }
+}
