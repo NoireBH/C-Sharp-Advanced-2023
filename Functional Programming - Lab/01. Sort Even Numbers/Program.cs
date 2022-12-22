70fd53fd0ffc58c50a0e2b4ab626af21d33a9b2c
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2022-12-22 15:12:39 +0200

    added projects

---
+++ Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace _01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = Console.ReadLine();
+            string outputPath = Console.ReadLine();
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+
+            
+        }
+    }
+}
---
+++ Streams, Files and Directories - Exercises/05. Copy Directory/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+           
+            string[] files = Directory.GetFiles(inputPath);
+
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileName(file);
+                var pathname = Path.GetFullPath(file);
+
+                File.Copy(pathname, outputPath + "//" + fileName);
+            }
+        }
+    }
+}
