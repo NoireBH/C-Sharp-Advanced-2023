21f54e64e996b2294191e5970c314a7f0a8ffef4
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2022-12-25 22:46:52 +0200

    added projects and solutions

---
+++ Defining Classes - Lab/01. Car/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "BMW";
+            car.Model = "MK4";
+            car.Year = 1984;
+
+            Console.WriteLine($"Make: {car.Make} \nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public string Make { get { return make; } set { make = value; } }
+        public string Model { get { return model; } set { model = value; } }
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
+
+
+    }
+}
---
+++ Defining Classes - Lab/02. Car Extension/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public  class Car
+        {
+            private string make;
+            private string model;
+            private int year;
+            private double fuelQuantity;
+            private double fuelConsumption;
+
+
+            public string Make { get { return make; } set { make = value; } }
+            public string Model { get { return model; } set { model = value; } }
+            public int Year
+            {
+                get
+                {
+                    return year;
+                }
+                set                               //this works too but its more rows
+                {
+                    year = value;
+                }
+            }
+
+            public double FuelQuantity { get { return fuelQuantity; } set { fuelQuantity = value; } }
+
+            public double FuelConsumption { get { return fuelConsumption; } set { fuelConsumption = value; } }
+
+            public void Drive(double distance)
+            {   
+                double leftFuel  = FuelQuantity - (fuelConsumption * distance);
+                
+
+                if (leftFuel < 0)
+                {
+                    Console.WriteLine("Not enough fuel to perform this trip!");
+                return;
+                }
+                    fuelQuantity -= distance * fuelConsumption;
+                
+                
+
+            }
+
+            public string WhoAmI()
+            {
+                return $"Make: {this.Make}\nModel: {this.Model}\nFuel: {this.fuelQuantity:F2}";
+
+            }
+
+
+        }
+}
