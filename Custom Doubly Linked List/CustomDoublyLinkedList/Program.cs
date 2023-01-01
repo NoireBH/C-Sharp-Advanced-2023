631af9e96ac013ce72665e0104286a805518cd8e
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-01-01 14:19:36 +0200

    added a custom class project

---
+++ Custom Doubly Linked List/CustomDoublyLinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+            doublyLinkedList.AddFirst(5);
+            doublyLinkedList.AddFirst(6);
+            doublyLinkedList.AddLast(7);
+            doublyLinkedList.ForEach();
+        }
+    }
+}
