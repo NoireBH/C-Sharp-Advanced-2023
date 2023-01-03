d47f9275cbe5a0d17c5d024d4b390ea2fed55a7b
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-01-03 12:23:04 +0200

    added projects and solutions

---
+++ Generics - Lab/02.ArrayCreator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(5, "Georgi");
+            int[] integers = ArrayCreator.Create(10, 10);
+
+            Console.WriteLine(string.Join(" ", strings));
+            Console.WriteLine(string.Join(" ", integers));
+        }
+    }
+}
