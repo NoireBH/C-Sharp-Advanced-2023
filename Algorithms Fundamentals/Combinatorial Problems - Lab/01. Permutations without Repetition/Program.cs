6b938c6e33add59c3504bbc1bbb25113fc9aa32d
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-01-08 12:58:10 +0200

    added projects

---
+++ Algorithms Fundamentals/Combinatorial Problems - Lab/01. Permutations without Repetition/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01._Permutations_without_Repetition
+{   
+    
+    internal class Program
+    {
+        public static bool[] used;
+        public static string[] elements;
+        public static string[] permutations;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(' ');
+            permutations = new string[elements.Length];
+            used = new bool[elements.Length];
+            
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {   
+            
+            if (index >= permutations.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (!used[i])
+                {   
+                    used[i] = true;
+                    permutations[index] = elements[i];
+                    Permute(index + 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
--- Algorithms Fundamentals/Recursion and Backtracking - Lab/07. Recursive Fibonacci/Program.cs
+++ Algorithms Fundamentals/Recursion and Backtracking - Lab/07. Recursive Fibonacci/Program.cs
@@ -6,7 +6,18 @@ namespace _07._Recursive_Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetFibonacchi(n)); 
+        }
+
+        private static int GetFibonacchi(int n)
+        {
+            if (n == 1 || n == 0)
+            {
+                return 1;
+            }
+
+           return  GetFibonacchi(n - 1) + GetFibonacchi(n - 2);
         }
     }
 }
