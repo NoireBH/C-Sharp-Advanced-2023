838f3e020d3834971ee423ee56c8819bcdcaf81c
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-01-13 12:23:22 +0200

    added projects

---
+++ Basic Algorithms - Exercise/04. Set Cover/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Set_Cover
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> universe = Console.ReadLine().Split(", ")
+                .Select(int.Parse).ToList();
+
+            int numberOfSets = int.Parse(Console.ReadLine());
+            var sets = new List<int[]>();
+
+            for (int i = 0; i < numberOfSets; i++)
+            {
+                int[] set = Console.ReadLine().Split(", ")
+                .Select(int.Parse).ToArray();
+                sets.Add(set);
+            }
+
+            var selectedSets = new List<int[]>();
+
+            while (universe.Count > 0)
+            {
+                var longestSet = sets.OrderByDescending(s => s.Count(x => universe.Contains(x)))
+                    .FirstOrDefault();
+                selectedSets.Add(longestSet);
+                sets.Remove(longestSet);
+
+                foreach (var item in longestSet)
+                {
+                    universe.Remove(item);
+                }
+            }
+
+            Console.WriteLine($"Sets to take ({selectedSets.Count}):");
+
+            foreach (var set in selectedSets)
+            {
+                Console.WriteLine($"{string.Join(", ", set)}");
+            }
+
+        }
+    }
+}
