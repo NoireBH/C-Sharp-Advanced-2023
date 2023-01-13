af6b51e6f84698b2234b17d08f112a304b366c6e
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-01-13 11:25:52 +0200

    added projects

---
+++ Basic Algorithms - Exercise/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Recursive_Array_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split()
+                .Select(int.Parse).ToArray();
+            Console.WriteLine(GetSum(array, 0)); 
+        }
+
+        private static int GetSum(int[] array,int index)
+        {
+            if (index >= array.Length - 1)
+            {
+                return array[index];
+
+            }
+            return array[index] + GetSum(array,index + 1);
+        }
+    }
+}
---
+++ Basic Algorithms - Exercise/03. Sum of Coins/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumOfCoins
+
+{
+
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int[] coins = Console.ReadLine().Split(", ")
+                .Select(int.Parse).ToArray();
+            int desiredSum = int.Parse(Console.ReadLine());
+
+            var selectedCoins = ChooseCoins(coins, desiredSum);
+
+            Console.WriteLine($"Number of coins to take: {selectedCoins.Values.Sum()}");
+
+            foreach (var item in selectedCoins)
+            {
+                Console.WriteLine($"{item.Value} coin(s) with value {item.Key}");
+            }
+
+        }
+
+        public static Dictionary<int, int> ChooseCoins(int[] coins, int targetSum)
+        {
+            Dictionary<int, int> result = new Dictionary<int, int>();
+
+            int sum = 0;
+            int indexOfCoins = 0;
+            int[] sortedCoins = coins.OrderByDescending(c => c).ToArray();
+
+            while (sum != targetSum && indexOfCoins < sortedCoins.Length)
+            {
+                int currentCoin = sortedCoins[indexOfCoins];
+                int remainder = targetSum - sum;
+                int numberOfCoins = remainder / currentCoin;
+
+                if (sum + currentCoin <= targetSum)
+                {
+                    result.Add(currentCoin, numberOfCoins);
+                    sum += currentCoin * numberOfCoins;
+                }
+                indexOfCoins++;
+            }
+
+            if (sum != targetSum)
+            {
+                throw new InvalidOperationException();
+            }
+
+
+
+            return result;
+        }
+    }
+}
