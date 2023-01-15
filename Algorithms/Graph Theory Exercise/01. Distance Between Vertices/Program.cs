8fdb08fe1bdd1ef29d1e83191179b24c448647a0
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-01-15 16:56:57 +0200

    Added Graph Traversal projects

--- Algorithms/Graph Theory Exercise/01. Distance Between Vertices/Program.cs
+++ Algorithms/Graph Theory Exercise/01. Distance Between Vertices/Program.cs
@@ -7,17 +7,16 @@ namespace _01._Distance_Between_Vertices
     internal class Program
     {
         private static Dictionary<int, List<int>> graph;
-        private static bool[] visited;
         static void Main(string[] args)
         {
             int numberOfVerticles = int.Parse(Console.ReadLine());
             int nOfPairsToSearchShortestPath = int.Parse(Console.ReadLine());
             graph = new Dictionary<int, List<int>>();
-            visited = new bool[graph.Count];
+            
 
             for (int i = 0; i < numberOfVerticles; i++)
             {
-                string[] line = Console.ReadLine().Split(":");
+                string[] line = Console.ReadLine().Split(":",StringSplitOptions.RemoveEmptyEntries);
 
                
                 int parent = int.Parse(line[0]);
@@ -38,12 +37,67 @@ namespace _01._Distance_Between_Vertices
             }
 
             for (int i = 0; i < nOfPairsToSearchShortestPath; i++)
-            {
+            {   
                 string[] pair = Console.ReadLine().Split("-");
                 int start = int.Parse(pair[0]);
                 int end = int.Parse(pair[1]);
+
+                var steps = BFS(start, end);
+
+                Console.WriteLine($"{{{ String.Join(", ", pair)}}} -> {steps}");
+                
+
             }
 
+        }
+
+        private static int BFS(int start, int end)
+        {
+            var visited = new HashSet<int> { start };
+            Queue<int> queue = new Queue<int>();
+            var parent = new Dictionary<int, int> { { start, -1 } };
+
+            queue.Enqueue(start);
+            visited.Add(start);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if (node == end)
+                {
+                    var steps = GetPath(end,parent);
+
+                    return steps;
+                }
+                foreach (var child in graph[node])
+                {
+                    if (visited.Contains(child))
+                    {
+                        continue;
+                    }
+
+                    parent[child] = node;
+                    visited.Add(child);
+                    queue.Enqueue(child);
+                }
+            }
+
+            return -1;
+        }
+
+        private static int GetPath(int end,Dictionary<int,int> parent)
+        {
+            int steps = 0;
+            var node = end;
+
+            while (node != -1)
+            {
+                steps++;
+                node = parent[node];
+            }
+
+            return steps - 1;
         }
     }
 }
---
+++ Algorithms/Graph Theory Exercise/02. Areas in Matrix/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Areas_in_Matrix
+{
+    internal class Program
+    {
+        private static char[,] graph;
+        private static bool[,] visited;
+        private static Dictionary<char,int> areas;
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            graph = new char[rows, cols];
+            visited = new bool[rows, cols];
+            areas = new Dictionary<char,int>();
+
+            for (int r = 0; r < graph.GetLength(0); r++)
+            {   
+                var elements = Console.ReadLine();
+
+                for (int c = 0; c < graph.GetLength(1); c++)
+                {
+                    graph[r, c] = elements[c];
+                }
+            }
+
+            for (int r = 0; r < graph.GetLength(0); r++)
+            {
+                for (int c = 0; c < graph.GetLength(1); c++)
+                {
+                    if (visited[r,c])
+                    {
+                        continue;
+                    }
+
+                    var currentNode = graph[r, c];
+                    DFS(r, c,currentNode);
+
+                    if (!areas.ContainsKey(currentNode))
+                    {
+                        areas[currentNode] = 1;
+                    }
+
+                    else
+                    {
+                        areas[currentNode]++;
+                    }
+                }
+            }
+            Console.WriteLine($"Areas: {areas.Values.Sum()}");
+
+            foreach (var area in areas.OrderBy(x => x.Key))
+            {
+                
+                Console.WriteLine($"Letter '{area.Key}' -> {area.Value}");
+            }
+
+        }
+
+        private static void DFS(int row, int col, char currentNode)
+        {
+            if (row <0 || row >= graph.GetLength(0) || col < 0 || col >= graph.GetLength(1))
+            {
+                return;
+            }
+
+            if (visited[row,col])
+            {
+                return;
+            }
+
+            if (graph[row,col] != currentNode)
+            {
+                return;
+            }
+
+            visited[row,col] = true;
+
+            DFS(row, col - 1, currentNode);
+            DFS(row, col + 1, currentNode);
+            DFS(row + 1, col , currentNode);
+            DFS(row - 1, col , currentNode);
+        }
+    }
+}
